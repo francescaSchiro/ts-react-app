7d9202af2f326c1d7986618ca64f0321a9dcd77b
De Filippis Francesco <fradef.ite@sisal.it>
2018-11-28 12:38:49 +0100

    FIX http-client class

--- src/utils/HttpClient.ts
+++ src/utils/HttpClient.ts
@@ -1,4 +1,5 @@
-import Axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import Axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse, AxiosError, AxiosPromise } from 'axios';
+import { merge } from 'lodash';
 
 const DEFAULT_AXIOS_REQUEST_CONFIG: AxiosRequestConfig = {
     baseURL: '/app/fake/',
@@ -14,29 +15,31 @@ export default class HttpClient {
     axios: AxiosInstance;
 
     constructor(config?: AxiosRequestConfig) {
-        this.axios = Axios.create(Object.assign({}, DEFAULT_AXIOS_REQUEST_CONFIG, config));
+        this.axios = Axios.create(merge({}, DEFAULT_AXIOS_REQUEST_CONFIG, config));
     }
 
     public get(
         url: string,
-        config: AxiosRequestConfig,
-        doneCb = (res: object) => res,
-        errorCb = (err: object) => err): Promise<any> {
+        config?: AxiosRequestConfig,
+        doneCb = (res: AxiosResponse): any => res,
+        errorCb = (err: AxiosError): any => err): AxiosPromise {
 
-        return this.axios.get(url, config)
-            .then(doneCb)
-            .catch(errorCb);
+        const promise: AxiosPromise = this.axios.get(url, config);
+        promise.then(doneCb, errorCb);
+
+        return promise;
     }
 
     public post(
         url: string,
-        data: object,
-        config: AxiosRequestConfig,
-        doneCb = (res: object) => res,
-        errorCb = (err: object) => err): Promise<any> {
+        data?: object,
+        config?: AxiosRequestConfig,
+        doneCb = (res: AxiosResponse): any => res,
+        errorCb = (err: AxiosError): any => err): AxiosPromise {
 
-        return this.axios.post(url, data, config)
-            .then(doneCb)
-            .catch(errorCb);
+        const promise: AxiosPromise = this.axios.post(url, data, config);
+        promise.then(doneCb, errorCb);
+
+        return promise;
     }
 }
