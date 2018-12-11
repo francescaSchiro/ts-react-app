cc102c6113e7d53e36653dd4e056878bf3d2e222
De Filippis Francesco <fradef.ite@sisal.it>
2018-12-11 16:29:24 +0100

    ADD interface to rootReducer

--- src/core/reducers.tsx
+++ src/core/reducers.tsx
@@ -1,23 +1,29 @@
 import { persistReducer, PersistedState } from 'redux-persist';
-import { connectRouter, RouterAction } from 'connected-react-router';
+import { connectRouter, RouterAction, RouterState } from 'connected-react-router';
 import { History } from 'history';
-import { combineReducers, Reducer } from 'redux';
+import { combineReducers, Reducer, ReducersMapObject } from 'redux';
 
 import { commonPersistConfig, protectedPersistConfig } from 'src/core/persist';
-import userReducer from 'src/shared/User/reducer';
-import appReducer from 'src/containers/App/reducer';
-import languageReducer from 'src/containers/LanguageProvider/reducer';
+import userReducer, { UserState } from 'src/shared/User/reducer';
+import appReducer, { AppState } from 'src/containers/App/reducer';
+import languageReducer, { LanguageProviderState } from 'src/containers/LanguageProvider/reducer';
 import { MssAction } from 'src/types/custom';
 
+interface Reducers extends ReducersMapObject {
+  router: Reducer<RouterState, RouterAction>,
+  language: Reducer<LanguageProviderState, MssAction>,
+  app: Reducer<AppState, MssAction>,
+  user: Reducer<UserState, MssAction>
+}
 
 export default function getRootReducer(history: History): Reducer<PersistedState, RouterAction | MssAction> {
 
-  const rootReducer = combineReducers({
+  const reducers: Reducers = {
     router: connectRouter(history),
     language: languageReducer,
     app: appReducer,
     user: persistReducer(protectedPersistConfig, userReducer)
-  });
+  };
 
-  return persistReducer(commonPersistConfig, rootReducer);
+  return persistReducer(commonPersistConfig, combineReducers(reducers));
 }
