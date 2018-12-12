2d0601af800abb770738be59b342759c44bc4980
De Filippis Francesco <fradef.ite@sisal.it>
2018-12-12 15:12:00 +0100

    ADD httpclient 'all' method

--- src/shared/HttpClient.tsx
+++ src/shared/HttpClient.tsx
@@ -12,27 +12,33 @@ const AXIOS_CONFIG: AxiosRequestConfig = {
 
 const axios: AxiosInstance = Axios.create(AXIOS_CONFIG);
 
-function errorHandler(error: AxiosError): AxiosError {
+const errorHandler = (error: AxiosError): AxiosError => {
   // tslint:disable-next-line:no-console
   console.error(`${error.name}: ${error.message}`);
   return error;
 }
 
-const get = (url: string, config?: AxiosRequestConfig): Promise<any> => {
+const get = (url: string, config?: AxiosRequestConfig): Promise<any> => { // TODO
   return axios.get(url, config).then(
     (res: AxiosResponse): any => res.data,
     errorHandler
   );
 }
 
-const post = (url: string, data?: object, config?: AxiosRequestConfig): Promise<any> => {
+const post = (url: string, data?: object, config?: AxiosRequestConfig): Promise<any> => { // TODO
   return axios.post(url, data, config).then(
     (res: AxiosResponse): any => res.data,
     errorHandler
   );
 }
 
+const all = (requests: Array<Promise<any>> = []): Promise<any> => { // TODO
+  return Axios.all(requests)
+    .then(Axios.spread((...params) => params));
+}
+
 export {
   get,
-  post
+  post,
+  all
 }
