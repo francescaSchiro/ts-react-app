09b34b01e76a8ca928b1da8dbd221c03d29e8e57
Schiro Francesca <francesca.schiro@sisal.it>
2018-12-13 14:37:24 +0100

    FIX Best Events Data on multiplelines if longer

--- src/components/HomePage/BestEventData.tsx
+++ src/components/HomePage/BestEventData.tsx
@@ -4,6 +4,10 @@ const BestEventData = styled.div`
   grid-column: 2 / 7;
   grid-row: 2 / 4;
 
+  min-width: 170px;
+  white-space: normal;
+  word-wrap:break-word;
+  line-height: 1.2em;
   display: flex;
   flex-direction: row;
   border-right: 1px solid rgba(225,225,225, .4);
