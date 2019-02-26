84aa37d5b6a9fc83a6b40705589c187649e1c0f8
Schiro Francesca <francesca.schiro@sisal.it>
2019-02-26 11:44:01 +0100

    ADD BetslipFooter

--- src/containers/Ticket/BetslipFooter/index.tsx
+++ src/containers/Ticket/BetslipFooter/index.tsx
@@ -4,16 +4,36 @@ import Wrapper from './Wrapper';
 import RowWrapper from './RowWrapper';
 import StakeAction from './StakeAction';
 import StakeImport from './StakeImport';
+import Column from './Column';
+import Label from './Label';
+import Value from './Value';
 
 
 
 const BetslipFooter = () => (
     <Wrapper>
+
         <RowWrapper>
             <StakeAction>-</StakeAction>
             <StakeImport>€ 3,00</StakeImport>
             <StakeAction>+</StakeAction>
         </RowWrapper>
+
+        <RowWrapper>
+            <Column>
+                <Label>Quota totale</Label>
+                <Value>2,70</Value>
+            </Column>
+            <Column>
+                <Label>Bonus multipla</Label>
+                <Value>€ 0,00</Value>
+            </Column>
+            <Column>
+                <Label>Vincita potenziale</Label>
+                <Value>€ 8,10</Value>
+            </Column>
+        </RowWrapper>
+
     </Wrapper>
 );
 
