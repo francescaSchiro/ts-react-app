338c1cab9b69e37de2c0b47ab4b0164718086cdf
Schiro Francesca <francesca.schiro@sisal.it>
2019-02-26 11:31:26 +0100

    ADD FooterStake actions

--- src/containers/Ticket/index.tsx
+++ src/containers/Ticket/index.tsx
@@ -4,12 +4,14 @@ import Wrapper from './Wrapper';
 import BetslipHead from './BetslipHead';
 import BetslipBody from './BetslipBody';
 import SettingsDefaultBlock from './SettingsDefaultBlock';
+import BetslipFooter from './BetslipFooter';
 
 const Ticket = () => (
     <Wrapper>
         <BetslipHead />
         <BetslipBody />
         <SettingsDefaultBlock />
+        <BetslipFooter />
     </Wrapper>
 );
 
