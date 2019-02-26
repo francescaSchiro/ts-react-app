7f4649e3822df3f388d270407eb979fbfc7109cd
Schiro Francesca <francesca.schiro@sisal.it>
2019-02-26 11:57:29 +0100

    FIX style ticket bg

--- src/containers/Ticket/Wrapper.tsx
+++ src/containers/Ticket/Wrapper.tsx
@@ -1,13 +1,8 @@
 import styled from '../../theme/default/index';
 
 const Wrapper = styled.div`
-    /* position: absolute; */
     margin-top:45px;
-    /* width: 100%; */
     height: calc(100vh - 45px);
-
-    background-color: pink;
-    color: black;
 `
 
 export default Wrapper;
