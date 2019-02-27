8803b9f7697c873d921491873fadb9ab9b56903d
Schiro Francesca <francesca.schiro@sisal.it>
2019-02-27 11:41:24 +0100

    ADD checked after when Fixed selected in Sistema

--- src/containers/Ticket/BetslipBody/BetslipBodyItem/FixedCheckbox.tsx
+++ src/containers/Ticket/BetslipBody/BetslipBodyItem/FixedCheckbox.tsx
@@ -5,6 +5,7 @@ interface IFixedCheckbox {
 }
 
 const FixedCheckbox = styled.div`
+    position: relative;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -16,20 +17,18 @@ const FixedCheckbox = styled.div`
     border-radius: 0 0 2px 2px ;
     opacity:1;
 
-    /* &::after {
-    box-sizing:border-box;
+    ${(props: IFixedCheckbox) => props.checked ? `
+    &::after {
+    content: '';
     position: absolute;
-    top:0;
-    left:0;
-    content:'';
-    height:16px;
-    width: 16px;
-    font-weight:bold;
-    color: #fff;
-    border: 2px solid #909090;
-    border-radius: 0 0 2px 2px ;
-    opacity:1;
-    z-index: 10;
-    } */
+    left: 50%;
+    top: 40%;
+    width: 6px;
+    height: 10px;
+    border-right: 2px solid #186cb5;
+    border-bottom: 2px solid #186cb5;
+    transform: translate(-50%,-50%) rotate(45deg) skew(11deg);
+    }`: null
+    }
 `
 export default FixedCheckbox;
