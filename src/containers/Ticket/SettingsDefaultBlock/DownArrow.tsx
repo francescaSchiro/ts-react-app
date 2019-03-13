eb19e9d37fc7db06ad1d48966abb554a89888ce4
Schiro Francesca <francesca.schiro@sisal.it>
2019-03-13 11:11:52 +0100

    ADD toggleInfo&Checklist in settingDefaultBlock

--- src/containers/Ticket/SettingsDefaultBlock/DownArrow.tsx
+++ src/containers/Ticket/SettingsDefaultBlock/DownArrow.tsx
@@ -1,10 +1,16 @@
 
 import styled from 'src/theme/default/index';
 
+interface IDownArrow {
+    showChecklist: boolean,
+}
+
 const DownArrow = styled.div`
     height: 8px;
     width: 15px;
-    background: url('https://m.sisal.it/scommesse-matchpoint/content/img/frecciaImpostazioniGiu.png?v=2.6.8') no-repeat;
+    background: ${ (props: IDownArrow) => props.showChecklist
+        ? `url('https://m.sisal.it/scommesse-matchpoint/content/img/frecciaImpostazioniSu.png?v=2.6.8') no-repeat;`
+        : `url('https://m.sisal.it/scommesse-matchpoint/content/img/frecciaImpostazioniGiu.png?v=2.6.8') no-repeat`};
     background-size: 14px;
     margin-right: 8px;
    
