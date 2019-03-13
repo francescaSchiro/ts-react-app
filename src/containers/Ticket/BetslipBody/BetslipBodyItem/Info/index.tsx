501728c8b6db1a9ad7df1218118953154cc08fc6
Schiro Francesca <francesca.schiro@sisal.it>
2019-03-13 15:50:47 +0100

    ADD OddChange cases to hardcode in  <Info>

--- src/containers/Ticket/BetslipBody/BetslipBodyItem/Info/index.tsx
+++ src/containers/Ticket/BetslipBody/BetslipBodyItem/Info/index.tsx
@@ -8,7 +8,20 @@ import DeleteIcon from 'src/containers/Ticket/DeleteIcon';
 import OddChange from './OddChange';
 
 
-
+/* incoming newOddValue + oddState @ checkOddState time interval: => 
+setState({
+    oldOddValue= this.state.currentOddValue,
+    currentOddValue = newOddValue (incoming one)
+})
+oddState: number {
+    -10: negative, 
+    0: closed, 
+    1: same, 
+    10: positive 
+}
+currentOddValue: number 
+oldOddValue: number // to print both in content string: `Quota cambiata da ${oldOddValue} a ${newOddValue}`
+*/
 
 class Info extends React.Component {
     public render() {
@@ -20,7 +33,7 @@ class Info extends React.Component {
                     Esito finale 1x2:
                     <Odd>
                         2 (-)
-                        <OddChange>(1.28)</OddChange>
+                        <OddChange change={'positive'}>(1.28)</OddChange>
                     </Odd>
                 </Game>
                 <MicroGame>Esito chiuso o sospeso</MicroGame>
