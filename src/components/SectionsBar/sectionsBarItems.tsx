5e4c4d4ebbd85a325dd033567def99b6d66846f7
Schiro Francesca <francesca.schiro@sisal.it>
2019-03-15 16:16:30 +0100

    FIX IsectionsBarItem

--- src/components/SectionsBar/sectionsBarItems.tsx
+++ src/components/SectionsBar/sectionsBarItems.tsx
@@ -1,4 +1,4 @@
-interface MySportListItems {
+interface IsectionsBarItem {
   active: boolean;
   key: number;
   label: string;
@@ -6,7 +6,7 @@ interface MySportListItems {
   iconText: () => number | string;
 }
 
-const sectionsBarItems: MySportListItems[] = [
+const sectionsBarItems: IsectionsBarItem[] = [
   {
     active: true,
     key: 0,
