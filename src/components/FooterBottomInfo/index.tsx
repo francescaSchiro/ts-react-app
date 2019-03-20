b193119ce5c81987182eb1a51625f2b33b73586f
Schiro Francesca <francesca.schiro@sisal.it>
2019-03-20 17:54:32 +0100

    FIX style FooterBottomInfoItem

--- src/components/FooterBottomInfo/index.tsx
+++ src/components/FooterBottomInfo/index.tsx
@@ -10,7 +10,7 @@ const FooterBottomInfoItem = styled.div`
     flex-direction: row;
     justify-content: center;
     align-items: center;
-    font-size: 6px;
+    font-size: 8px;
     white-space: pre-wrap;
     line-height: 10px;
 `
@@ -21,7 +21,7 @@ const Link = styled.a`
 
 const FooterBottomInfo = () => (
     <Wrapper>
-        <FooterBottomInfoItem style={{ textTransform: 'uppercase', paddingBottom: '5px' }}>IL GIOCO é VIETATO AI MINORI E PUò CAUSARE DIPENDENZA PATOLOGICA</FooterBottomInfoItem>
+        <FooterBottomInfoItem style={{ textTransform: 'uppercase', paddingBottom: '5px', fontSize: '6px' }}>IL GIOCO é VIETATO AI MINORI E PUò CAUSARE DIPENDENZA PATOLOGICA</FooterBottomInfoItem>
         <FooterBottomInfoItem>Sisal © Sisal Entertainment S.p.A. partita IVA 02433760135 - Conc. GAD: 15155></FooterBottomInfoItem>
         <FooterBottomInfoItem>Questo sito e le piattaforme di gioco utilizzano <Link href='https://m.sisal.it/societa/cookies?backto=https://m.sisal.it/scommesse-matchpoint/&siteid=5'>cookies</Link></FooterBottomInfoItem>
     </Wrapper>
