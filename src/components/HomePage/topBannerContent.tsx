2ae738def69cae170a57a98cbef39ad35bf84d72
Schiro Francesca <francesca.schiro@sisal.it>
2019-03-21 10:49:28 +0100

    FIX TopBanner

---
+++ src/components/HomePage/topBannerContent.tsx
@@ -0,0 +1,13 @@
+export interface ITopBanner {
+  imgUrl: string,
+  url: string
+  description: string
+}
+
+const topBannerContent: ITopBanner = {
+  imgUrl: 'https://www.clondalkinleisure.com/wp-content/uploads/2016/03/img-allweather-banner02.jpg',
+  url: 'https://mtest-liferay.sisal.it/scom-matchpoint/promozioni-e-novita',
+  description: 'Questa funzionalità ti permette di concludere la scommessa live in modo veloce anche se le quote si aggiornano in modo non rilevante.',
+};
+
+export default topBannerContent;
